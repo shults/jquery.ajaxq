e841a51c879076372c245602d33f7551a065617d
Yaroslav Kotsur <yarikkotsur@gmail.com>
2015-06-22 18:18:48 +0200

    * fix bug with bandwith abort

--- src/Queue.js
+++ src/Queue.js
@@ -5,25 +5,37 @@ var Queue = (function() {
 
     this._bandwidth = parseInt(bandwidth || 1, 10);
     if (this._bandwidth < 1) throw "Bandwidth can\'t be less then 1";
-    this._requests = [];
+    
+    this._started = [];
+    this._queue = [];
   };
+
+  function _runNext(request) {
+    var 
+      removeIndex = this._started.indexOf(request),
+      nextRequest = this._queue.shift();
+    
+    if (removeIndex !== -1) {
+      this._started.splice(removeIndex, 1);  
+    }
+
+    if (typeof nextRequest !== 'undefined') {
+      nextRequest.always(_runNext.bind(this, nextRequest));
+      nextRequest.run();
+    }
+  }
 
   $.extend(Queue.prototype, {
     ajax: function(url, settings) {
       var request = new Request(url, settings);
 
-      if (this._requests.length < this._bandwidth) {
+      if (this._started.length < this._bandwidth) {
+        this._started.push(request);
+        request.always(_runNext.bind(this, request));
         request.run();
       } else {
-
-        $.each(this._requests, function(i, _request) {
-          _request.always( request.run.bind(request) );
-        });
-
-        this._requests.shift();
+        this._queue.push(request);
       }
-
-      this._requests.push(request);
 
       return request;
     },
